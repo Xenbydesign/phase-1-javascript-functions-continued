3f4e8faab70dd5d9559c0139811a6fc38fb74037
Xennia Contreras <Xenbydesign@gmail.com>
2023-10-31 17:52:36 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(fun ="roller-skate") {
+    return `This Saturday, I want to ${fun}!`;
+}
+
+const mondayWork = function (office = 'go to the office') {
+    return `This Monday, I will ${office}.`;
+
+}
+function wrapAdjective(flair ='*'){
+    return function (innerFunction = 'special'){
+    return `You are ${flair}${innerFunction}${flair}!`
+    };
+}
